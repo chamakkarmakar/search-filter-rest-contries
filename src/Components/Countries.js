6f16fb18d71f76da444a7d0388753c43149e74ea
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2023-01-26 22:01:04 +0600

    load all countries data using fetch api

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card } from 'react-bootstrap';
 
 const Countries = () => {
+    const [countries,setCountries]=useState([]);
+
+    useEffect(()=>{
+        fetch("https://restcountries.com/v3.1/all")
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
     
   return (
     <Card style={{ width: '18rem' }}>
